52e6db79f4728812ebb5b87105f7ff552214cd5f
Sovanna Hing <sovanna.hing@gmail.com>
2012-03-25 19:57:49 +0200

    clean

--- SHelloWorldLibraryIOS/build/SHelloWorldLibraryIOS.build/Release-iphonesimulator/SHelloWorldLibraryIOS.build/Objects-normal/i386/SHelloWorldLibraryIOS.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/sovannahing/Documents/personalWorks/_development/_github/_tuto/universal-library-iOS/SHelloWorldLibraryIOS/SHelloWorldLibraryIOS/SHelloWorldLibraryIOS.m \
-  /Users/sovannahing/Documents/personalWorks/_development/_github/_tuto/universal-library-iOS/SHelloWorldLibraryIOS/SHelloWorldLibraryIOS/SHelloWorldLibraryIOS.h
